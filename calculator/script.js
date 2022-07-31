4534ec538b7ea708a9372f41af53424b24f457e0
Domnann <domnangambo@gmail.com>
2022-07-31 23:18:28 +0100

    changes made to script.js

---
+++ calculator/script.js
@@ -0,0 +1,6 @@
+const numBtn = document.querySelectorAll('[data-number]')
+const operationBtn = document.querySelectorAll('[data-operation]')
+const equalsBtn = document.querySelectorAll('[data-equals]')
+const clearBtn = document.querySelectorAll('[data-all-clear]')
+const previousOperandTextElement= document.querySelectorAll('[previous-output]')
+const currentOperandTextElement = document.querySelectorAll('[current-output]')
